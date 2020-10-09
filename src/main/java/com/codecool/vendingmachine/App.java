06999eb8a86797cb079b4ff406d16418bba2c691
Grelory <gmpwm99@gmail.com>
2020-10-09 02:58:22 +0200

    implement application in main method

--- src/main/java/com/codecool/vendingmachine/App.java
+++ src/main/java/com/codecool/vendingmachine/App.java
@@ -1,9 +1,24 @@
 package com.codecool.vendingmachine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
 
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws FileNotFoundException {
+        BankMachine bankMachine = new BankMachine();
+        ProductMachine productMachine = new ProductMachine();
+        Scanner scanner;
+        if (args.length == 1) {
+            String file = App.class.getClassLoader().getResource(args[0]).getFile();
+            scanner = new Scanner(new File(file));
+        } else {
+            scanner = new Scanner(System.in);
+        }
+        IOMachine ioMachine = new IOMachine(scanner);
+        VendingMachine vendingMachine = new VendingMachine(bankMachine, ioMachine, productMachine);
+        vendingMachine.run();
     }
 
 }
